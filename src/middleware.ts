44eabb99aeb3920a88f5373d6cbff5b64f8cda70
Dennis Miasoutov <dennis.miasoutov@mocoding.com>
2017-01-02 15:54:49 -0500

    Removed automataMiddleware from default export.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,24 @@
 import { IAction, IAutomataState } from './common';
 import * as Redux from 'redux';
 
-const automataMiddleware: Redux.Middleware =
-    <S>(api: Redux.MiddlewareAPI<S>) =>
-        (next: Redux.Dispatch<S>) =>
-            (action: Redux.Action) => {
-                var smAction = action as IAction<S>
-                if (smAction.__sm__ === undefined)
-                    return next(action);
-                
-                var dispatching = true;
-                smAction.context = {
-                    dispatch: (a: any) => { 
-                        if (dispatching) 
-                            setTimeout(()=>api.dispatch(a));
-                        else
-                            api.dispatch(a);
-                    }
-                }
-                var defered = next(smAction);
-                dispatching = false;
-                return defered;
-            }
+export function automataMiddleware<S>(api: Redux.MiddlewareAPI<S>): (next: Redux.Dispatch<S>) => Redux.Dispatch<S> {
+    return (next: Redux.Dispatch<S>) =>
+        (action: Redux.Action) => {
+            var smAction = action as IAction<S>
+            if (smAction.__sm__ === undefined)
+                return next(action);
 
-export default automataMiddleware;
+            var dispatching = true;
+            smAction.context = {
+                dispatch: (a: any) => {
+                    if (dispatching)
+                        setTimeout(() => api.dispatch(a));
+                    else
+                        api.dispatch(a);
+                }
+            }
+            var defered = next(smAction);
+            dispatching = false;
+            return defered;
+        }
+}
